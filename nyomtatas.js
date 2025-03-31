22205a4e5eb281a6d1c1d414bbacda662b9639a4
schaferv <schaferv.24d@acsjszki.hu>
2025-03-31 10:35:16 +0200

    asd
    asd

--- nyomtatas.js
+++ nyomtatas.js
@@ -1,12 +1,17 @@
 function kalkulal(){
+
     //Űrlapadatok
-    const szelesseg=297;
-    const magassag=420;
-    const papir=document.getElementById('papirtipus').value;
+    const szelesseg= document.getElementById('szelesseg').value;
+    const magassag= document.getElementById('magassag').value;
+    const papir= document.getElementById('papirtipus').value;
+
     //Számítások
     let terulet=Math.round((szelesseg*magassag)/10000);    
-    let koltseg=terulet*papir;
-    //Megjelenítés
-    document.getElementById('valasz').style.visibility = "visible";
-}
+    let koltseg=terulet * papir;
 
+    //Megjelenítés
+    document.getElementById('terulet').innerHTML = terulet; 
+    document.getElementById('papir').innerHTML = papir;
+    document.getElementById('koltseg').innerHTML = koltseg;
+    document.getElementById('valasz').style.visibility = "visible";
+}document.getElementById('valasz').style.backgroundColor = "green"
